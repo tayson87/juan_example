783fe9640d5dc1f7f53ebaeeb494ccb725760ed2
tayson87 <juanpablomartinez37@gmail.com>
2022-03-08 23:24:09 -0500

    update code 6

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ sequelize
   .then(() => console.log('Database synced'))
   .catch((err) => console.log(err));
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
   console.log('Express app running');
 });
